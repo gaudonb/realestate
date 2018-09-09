f1c0c5cbbb63a140e36d7e47444c31d6c6973742
Brent Gaudon <gaudons@msn.com>
2018-09-09 19:27:12 -0400

    Finished up landing page, started listings

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -2,20 +2,24 @@ $(function(){
 	$("#sidebar-toggler").click(function(){
 		if($("#sidebar").hasClass("sidebar-open")){
 			$("#sidebar").removeClass("sidebar-open");
+			$("#modular").removeClass("modular-show");
+			$("body").removeClass("disable-h-scroll")
 		}else{
-			$("#sidebar").addClass("sidebar-open");
+			$("#sidebar").addClass("sidebar-open");	
+			$("#modular").addClass("modular-show");	
+			$("body").addClass("disable-h-scroll")
 		}	
 	});
 
 	$("#signup-butt").hover(function(){
-		$("#signup-text").addClass("signup-text-open")
+		$("#signup-text").addClass("signup-text-open");
 	},function(){
-		$("#signup-text").removeClass("signup-text-open")
+		$("#signup-text").removeClass("signup-text-open");
 	});
 
 	$("#signin-butt").hover(function(){
-		$("#signin-text").addClass("signin-text-open")
+		$("#signin-text").addClass("signin-text-open");
 	},function(){
-		$("#signin-text").removeClass("signin-text-open")
+		$("#signin-text").removeClass("signin-text-open");
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  	res.render('index', { title: 'Welcome' });
+});
+
+router.get('/listings', function(req, res, next){
+	res.render('listings/get', {title:'Listings'});
 });
 
 module.exports = router;
